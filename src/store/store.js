2e42b4500bbc532fab729dcf6f33daae9d46a6f4
hgs-study <hgstudy_@naver.com>
2021-10-25 18:59:47 +0900

    Attribute Module

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as getters from './getters'
+import * as mutations from './mutations'
 
 // use: 뷰 모든 영역에 특정 기능을 사용하고 싶을 때 사용 
 Vue.use(Vuex);
@@ -23,30 +25,6 @@ export const store = new Vuex.Store({
     state : {
         todoItems : storage.fetch()
     },
-    getters : {
-        storedTodoItems(state){
-            return state.todoItems;
-        }
-    },
-    //mutations에서 state 접근 시 this. 가 아닌 state. 로 접근해야한다.
-    mutations : {
-        addOneItem(state, todoItem){
-            const obj = {completed : false, item : todoItem};
-            localStorage.setItem(todoItem, JSON.stringify(obj));
-            state.todoItems.push(obj);
-        },
-        removeOneItem(state,payload){
-            localStorage.removeItem(payload.todoItem.item);
-            state.todoItems.splice(payload.index,1);
-        },
-        toggleOneItem(state,payload){
-            state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
-            localStorage.removeItem(payload.todoItem.item);
-            localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
-        },
-        clearAllItems(state){
-            localStorage.clear();
-            state.todoItems = [];
-        }
-    }
+    getters,
+    mutations
 });
