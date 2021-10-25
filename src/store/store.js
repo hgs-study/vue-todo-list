09b54d833fe4c2e383b3da95ee84fb559ffd608c
hgs-study <hgstudy_@naver.com>
2021-10-26 00:25:09 +0900

    Update Project Module

--- src/store/store.js
+++ src/store/store.js
@@ -1,30 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as getters from './getters'
-import * as mutations from './mutations'
+import todoApp from './modules/todoApp'
 
 // use: 뷰 모든 영역에 특정 기능을 사용하고 싶을 때 사용 
 Vue.use(Vuex);
 
-const storage = {
-    fetch(){
-        const arr = [];
-        if(localStorage.length>0){
-            for(let i = 0; i <localStorage.length ; i++){
-                if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-                    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-                    // this.todoItems.push(localStorage.key(i));          
-                }
-            }
-        }
-        return arr;
-    }
-};
+// const storage = {
+//     fetch(){
+//         const arr = [];
+//         if(localStorage.length>0){
+//             for(let i = 0; i <localStorage.length ; i++){
+//                 if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+//                     arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+//                     // this.todoItems.push(localStorage.key(i));          
+//                 }
+//             }
+//         }
+//         return arr;
+//     }
+// };
 
 export const store = new Vuex.Store({
-    state : {
-        todoItems : storage.fetch()
-    },
-    getters,
-    mutations
+    modules : {
+        todoApp
+    }
 });
